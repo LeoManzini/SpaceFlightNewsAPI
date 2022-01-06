0f17deed1a1acea5b50ee1d4fa3387a9687ff8c5
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-05 23:24:01 -0300

    Starting work on project configurations

---
+++ space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/configurations/SpaceFlightsBatchConfiguration.java
@@ -0,0 +1,7 @@
+package br.com.leomanzini.space.flights.batch.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpaceFlightsBatchConfiguration {
+}
