7698936712dcd5e87f91d281334d1734c500cc15
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-05 23:02:17 -0300

    Adding the batch project and renaming the api one

---
+++ space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/SpaceFlightsBatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.leomanzini.space.flights.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceFlightsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceFlightsBatchApplication.class, args);
+	}
+
+}
---
+++ space-flights-batch/src/test/java/br/com/leomanzini/space/flights/batch/SpaceFlightsBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leomanzini.space.flights.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceFlightsBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
