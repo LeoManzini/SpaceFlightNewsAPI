26834735859ea87aba8abf24bac31ad6a53e7483
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-06 19:24:41 -0300

    Adding the batch project

--- space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/SpaceFlightsBatchApplication.java
+++ space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/SpaceFlightsBatchApplication.java
@@ -1,13 +1,18 @@
 package br.com.leomanzini.space.flights.batch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpaceFlightsBatchApplication {
+public class SpaceFlightsBatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpaceFlightsBatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
 }
--- space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/configurations/SpaceFlightsBatchConfiguration.java
+++
@@ -1,7 +0,0 @@
-package br.com.leomanzini.space.flights.batch.configurations;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SpaceFlightsBatchConfiguration {
-}
