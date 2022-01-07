43bdb23a65616c478c04db3de00ee1983c4651c7
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-06 23:33:34 -0300

    Adding @Repository annotation

--- space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/repository/LaunchesRepository.java
+++ space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/repository/LaunchesRepository.java
@@ -2,6 +2,8 @@ package br.com.leomanzini.space.flights.batch.repository;
 
 import br.com.leomanzini.space.flights.batch.model.Launches;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LaunchesRepository extends JpaRepository<Launches, String> {
 }
