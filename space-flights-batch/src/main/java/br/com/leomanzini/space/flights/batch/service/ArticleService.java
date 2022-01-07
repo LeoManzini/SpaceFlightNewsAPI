add78ad817f8c486e1a37c82843a90abb0a53343
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-06 23:37:46 -0300

    Creating service class

--- space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/service/ArticleService.java
+++ space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/service/ArticleService.java
@@ -1,4 +1,20 @@
 package br.com.leomanzini.space.flights.batch.service;
 
+import br.com.leomanzini.space.flights.batch.repository.ArticleRepository;
+import br.com.leomanzini.space.flights.batch.repository.EventsRepository;
+import br.com.leomanzini.space.flights.batch.repository.LaunchesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private EventsRepository eventsRepository;
+
+    @Autowired
+    private LaunchesRepository launchesRepository;
 }
