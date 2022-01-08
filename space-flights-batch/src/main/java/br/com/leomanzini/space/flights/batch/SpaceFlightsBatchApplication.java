a0e80019c21990e4b2e7fc06a165b15eeea8b68f
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-08 00:51:31 -0300

    Adding ModelMapper

--- space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/SpaceFlightsBatchApplication.java
+++ space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/SpaceFlightsBatchApplication.java
@@ -1,10 +1,12 @@
 package br.com.leomanzini.space.flights.batch;
 
 import br.com.leomanzini.space.flights.batch.service.ArticleService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpaceFlightsBatchApplication implements CommandLineRunner {
--- space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/service/ArticleService.java
+++ space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/service/ArticleService.java
@@ -2,13 +2,12 @@ package br.com.leomanzini.space.flights.batch.service;
 
 import br.com.leomanzini.space.flights.batch.dto.ArticlesResponseDTO;
 import br.com.leomanzini.space.flights.batch.model.Article;
-import br.com.leomanzini.space.flights.batch.model.Events;
-import br.com.leomanzini.space.flights.batch.model.Launches;
 import br.com.leomanzini.space.flights.batch.repository.ArticleRepository;
 import br.com.leomanzini.space.flights.batch.repository.EventsRepository;
 import br.com.leomanzini.space.flights.batch.repository.LaunchesRepository;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,30 +19,29 @@ import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ArticleService {
 
     @Autowired
-    private ArticleRepository articleRepository;
+    private ModelMapper modelMapper;
 
     @Autowired
+    private ArticleRepository articleRepository;
+    @Autowired
     private EventsRepository eventsRepository;
-
     @Autowired
     private LaunchesRepository launchesRepository;
 
     @Value("${space.flights.api.context}")
     private String applicationContext;
-
     @Value("${space.flights.api.articles.count}")
     private String countArticles;
-
     @Value("${space.flights.api.all.articles}")
     private String allArticles;
 
+    // TODO colocar service e service impl
     // TODO montar rotina de insercao e verificacao de dados existentes na base
     // TODO montar rotina de verificacao se a API recebeu novos artigos e persistir os mesmos no banco,
     //  usar tabela a parte para guardar os dados de registros inseridos e um campo na tabela Articles para saber se foi inserido por user ou API
@@ -52,31 +50,31 @@ public class ArticleService {
         try {
             List<ArticlesResponseDTO> receivedDtoObject = getSpaceFlightsArticles();
             receivedDtoObject.forEach(item -> {
-                Article insertObject = new Article();
-
-                insertObject.setId(item.getId());
-                insertObject.setFeatured(item.getFeatured());
-                insertObject.setTitle(item.getTitle());
-                insertObject.setUrl(item.getUrl());
-                insertObject.setImageUrl(item.getImageUrl());
-                insertObject.setNewsSite(item.getNewsSite());
-                insertObject.setSummary(item.getSummary());
-                insertObject.setPublishedAt(item.getPublishedAt());
-
-                insertObject.setLaunches(new ArrayList<>());
-                insertObject.setEvents(new ArrayList<>());
-
-                item.getLaunches().forEach(items -> {
-                    insertObject.getLaunches().add(Launches.builder().id(items.getId()).provider(items.getProvider()).build());
-                });
-
-                item.getEvents().forEach(items -> {
-                    insertObject.getEvents().add(Events.builder().id(items.getId()).provider(items.getProvider()).build());
-                });
-
-                eventsRepository.saveAll(insertObject.getEvents());
-                launchesRepository.saveAll(insertObject.getLaunches());
-                articleRepository.save(insertObject);
+                Article insertObject = modelMapper.map(item, Article.class);
+                System.out.println(insertObject);
+//                insertObject.setId(item.getId());
+//                insertObject.setFeatured(item.getFeatured());
+//                insertObject.setTitle(item.getTitle());
+//                insertObject.setUrl(item.getUrl());
+//                insertObject.setImageUrl(item.getImageUrl());
+//                insertObject.setNewsSite(item.getNewsSite());
+//                insertObject.setSummary(item.getSummary());
+//                insertObject.setPublishedAt(item.getPublishedAt());
+//
+//                insertObject.setLaunches(new ArrayList<>());
+//                insertObject.setEvents(new ArrayList<>());
+//
+//                item.getLaunches().forEach(items -> {
+//                    insertObject.getLaunches().add(Launches.builder().id(items.getId()).provider(items.getProvider()).build());
+//                });
+//
+//                item.getEvents().forEach(items -> {
+//                    insertObject.getEvents().add(Events.builder().id(items.getId()).provider(items.getProvider()).build());
+//                });
+//
+//                eventsRepository.saveAll(insertObject.getEvents());
+//                launchesRepository.saveAll(insertObject.getLaunches());
+//                articleRepository.save(insertObject);
             });
         } catch (Exception e) {
             e.printStackTrace();
