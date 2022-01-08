425f94587a6718c72848aed75fa9245f2dd34601
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-08 17:36:02 -0300

    Entity for control articles at database

--- space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/service/ArticleService.java
+++ space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/service/ArticleService.java
@@ -43,8 +43,6 @@ public class ArticleService {
     @Value("${space.flights.api.all.articles}")
     private String allArticles;
 
-    // TODO criar classes utils
-    // TODO criar enum para return codes
     // TODO montar rotina de insercao e verificacao de dados existentes na base
     // TODO montar rotina de verificacao se a API recebeu novos artigos e persistir os mesmos no banco,
     //  usar tabela a parte para guardar os dados de registros inseridos e um campo na tabela Articles para saber se foi inserido por user ou API
