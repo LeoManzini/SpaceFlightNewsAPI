120c2819845c2604952aa452430eeea91930cddb
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-07 22:08:02 -0300

    Working persistence example

--- space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/SpaceFlightsBatchApplication.java
+++ space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/SpaceFlightsBatchApplication.java
@@ -1,11 +1,21 @@
 package br.com.leomanzini.space.flights.batch;
 
+import br.com.leomanzini.space.flights.batch.model.Article;
+import br.com.leomanzini.space.flights.batch.model.Events;
+import br.com.leomanzini.space.flights.batch.model.Launches;
+import br.com.leomanzini.space.flights.batch.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class SpaceFlightsBatchApplication implements CommandLineRunner {
+
+	@Autowired
+	private ArticleService articleService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpaceFlightsBatchApplication.class, args);
@@ -13,6 +23,21 @@ public class SpaceFlightsBatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		Article article = new Article();
+		article.setFeatured(true);
+		article.setTitle("Leonardo");
+		article.setUrl("leonardo.com");
+		article.setImageUrl("www.leonardo.com");
+		article.setNewsSite("Google");
+		article.setSummary("Sumario");
+		article.setPublishedAt("Hoje");
+		article.setLaunches(new ArrayList<>());
+		article.setEvents(new ArrayList<>());
+		article.getLaunches().add(Launches.builder().id("2000").provider("Eu mesmo").build());
+		article.getEvents().add(Events.builder().id(1L).provider("Eu mesmo").build());
+
+		System.out.println(article);
+
+		articleService.insertNewArticle(article);
 	}
 }
--- space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/service/ArticleService.java
+++ space-flights-batch/src/main/java/br/com/leomanzini/space/flights/batch/service/ArticleService.java
@@ -1,5 +1,6 @@
 package br.com.leomanzini.space.flights.batch.service;
 
+import br.com.leomanzini.space.flights.batch.model.Article;
 import br.com.leomanzini.space.flights.batch.repository.ArticleRepository;
 import br.com.leomanzini.space.flights.batch.repository.EventsRepository;
 import br.com.leomanzini.space.flights.batch.repository.LaunchesRepository;
@@ -17,4 +18,10 @@ public class ArticleService {
 
     @Autowired
     private LaunchesRepository launchesRepository;
+
+    public void insertNewArticle(Article article) {
+        eventsRepository.saveAll(article.getEvents());
+        launchesRepository.saveAll(article.getLaunches());
+        articleRepository.save(article);
+    }
 }
