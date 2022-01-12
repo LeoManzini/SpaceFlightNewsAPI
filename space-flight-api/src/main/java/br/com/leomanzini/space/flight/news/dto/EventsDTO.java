dec22306295f15dcba89e5012b45a5b9a84ddcfa
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-11 21:35:49 -0300

    Dto implementing serializable

--- space-flight-api/src/main/java/br/com/leomanzini/space/flight/news/dto/EventsDTO.java
+++ space-flight-api/src/main/java/br/com/leomanzini/space/flight/news/dto/EventsDTO.java
@@ -3,12 +3,13 @@ package br.com.leomanzini.space.flight.news.dto;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class EventsDTO {
+public class EventsDTO implements Serializable {
 
     @NotEmpty
     @EqualsAndHashCode.Include
