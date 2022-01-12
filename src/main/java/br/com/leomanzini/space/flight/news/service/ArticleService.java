c4259de222a59e61473c10c6020b2a4ca4984054
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-11 22:52:36 -0300

    Load finAll errors

--- src/main/java/br/com/leomanzini/space/flight/news/service/ArticleService.java
+++ src/main/java/br/com/leomanzini/space/flight/news/service/ArticleService.java
@@ -1,23 +1,23 @@
 package br.com.leomanzini.space.flight.news.service;
 
+import br.com.leomanzini.space.flight.news.dto.ArticlesDTO;
 import br.com.leomanzini.space.flight.news.model.Article;
 import br.com.leomanzini.space.flight.news.repository.ArticleRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
+@AllArgsConstructor
 public class ArticleService {
 
     @Autowired
     private final ArticleRepository articleRepository;
 
-    public ArticleService(ArticleRepository articleRepository) {
-        this.articleRepository = articleRepository;
-    }
-
-    public List<Article> findAll() {
-        return articleRepository.findAll();
+    public Page<ArticlesDTO> findAll(Pageable pageable) {
+        Page<Article> articleList = articleRepository.findAll(pageable);
+        return articleList.map(article -> new ArticlesDTO(article));
     }
 }
--- src/main/java/br/com/leomanzini/space/flight/news/utils/beans/ModelMapperMethods.java
+++ src/main/java/br/com/leomanzini/space/flight/news/utils/beans/ModelMapperMethods.java
@@ -2,14 +2,32 @@ package br.com.leomanzini.space.flight.news.utils.beans;
 
 import br.com.leomanzini.space.flight.news.dto.ArticlesDTO;
 import br.com.leomanzini.space.flight.news.model.Article;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ModelMapperMethods {
 
     @Autowired
-    private org.modelmapper.ModelMapper modelMapper;
+    private final ModelMapper modelMapper;
+
+    public ModelMapperMethods(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
+    }
+
+    public List<ArticlesDTO> entityListToDtoList (List<Article> articleList) {
+        return articleList.stream()
+                .map(entity -> modelMapper.map(entity, ArticlesDTO.class))
+                .collect(Collectors.toList());
+    }
 
     public Article dtoToEntity(ArticlesDTO articleDto) {
         return modelMapper.map(articleDto, Article.class);
